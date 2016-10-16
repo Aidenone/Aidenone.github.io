318c7993f8744d6d3e2b3f118bdd1593235a11c8
Aiden <aidenone.lucas@gmail.com>
2016-10-17 00:36:48 +0200

    no message

--- test.js
+++ test.js
@@ -1,11 +1,26 @@
 var $window = $(window),
 $stickyEl = $('#test'),
+$stickyEl2 = $('#test-shadow'),
 elTop = $stickyEl.offset().top;
 
 $window.scroll(function() {
     $stickyEl.toggleClass('sticky', $window.scrollTop() > elTop);
+    $stickyEl.removeClass('displayed');
+ 	$("#test2").css("display","none");
+ 	if($(".menu-icon").hasClass('fa-times')){
+ 		$(".menu-icon").addClass('fa-bars');
+ 		$(".menu-icon").removeClass('fa-times');
+ 	}
 });
-$stickyEl.click(function() {
+$(".menu-icon").click(function() {
   $stickyEl.toggleClass('displayed');
+  $("#test2").toggle();
+  if($(".menu-icon").hasClass('fa-bars')){
+  	$(".menu-icon").addClass('fa-times');
+  	$(".menu-icon").removeClass('fa-bars');
+  } else {
+  	$(".menu-icon").addClass('fa-bars');
+  	$(".menu-icon").removeClass('fa-times');
+  }
   //nav.displayed;
 });
